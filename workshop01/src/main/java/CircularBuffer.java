5a351c45966c2cf896a56ad4a30dd6f1d074d536
up1 <somkiat.p@gmail.com>
2021-02-08 12:11:54 +0700

    Create buffer and get size after created

--- workshop01/src/main/java/CircularBuffer.java
+++ workshop01/src/main/java/CircularBuffer.java
@@ -1,3 +1,22 @@
+import static java.lang.System.out;
+
 public class CircularBuffer {
-    // TODO
+    private String[] buffers;
+
+    public static void main(String[] args) {
+        CircularBuffer circularBuffer = new CircularBuffer();
+        // Create buffer : default size = 10
+        circularBuffer.create();
+        // Size of buffer
+        int size = circularBuffer.getSize();
+        out.println(size);
+    }
+
+    private int getSize() {
+        return buffers.length;
+    }
+
+    private void create() {
+        buffers = new String[10];
+    }
 }
